ecc2f96813c441ef4575d042516a38e955c89cda
ians hds <ven.herdiansyah@bankbsi.co.id>
2025-03-25 15:49:18 +0700

    chore : setup depedency

--- src/main/java/id/co/warehouse/application/WarehouseServiceApplication.java
+++ src/main/java/id/co/warehouse/application/WarehouseServiceApplication.java
@@ -2,8 +2,12 @@ package id.co.warehouse.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication()
+@EntityScan(basePackages = "id.co.warehouse.application.model")
+@EnableJpaRepositories(basePackages = "id.co.warehouse.application.repository")
 public class WarehouseServiceApplication {
 
 	public static void main(String[] args) {
